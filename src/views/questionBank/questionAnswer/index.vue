01515a3cd17962b997232be13ef7651d787ae94b
cmx-ops <l>
2023-04-10 17:17:50 +0800

    1

--- src/views/questionBank/questionAnswer/index.vue
+++ src/views/questionBank/questionAnswer/index.vue
@@ -1,17 +1,43 @@
 <template>
-<div>
-  <h1>
-    答案管理
-  </h1>
-</div>
+  <div>
+    <!--服务器会在请求头找到referrer信息，查看请求是否来自本站，如果不是就返回403。从而实现资源防盗-->
+    <meta name="referrer" content="no-referrer">
+    <h1>
+      答案管理
+    </h1>
+
+    <div>
+      <div>
+        <!--previewOnly 只预览不编辑-->
+        <MdEditor v-if="previewOnly" v-model="article" previewOnly/>
+        <!--具有编辑功能-->
+        <MdEditor v-if="!previewOnly" v-model="article"/>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script>
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
 
-defineOptions({
-  name: "QuestionAnswer"
-});
+export default {
 
+  components: {
+    MdEditor
+  },
+  data() {
+    return {
+      previewOnly:false,
+      article: "# 111"
+    }
+  },
+  created() {
+
+  },
+  methods: {},
+
+}
 </script>
 
 <style scoped>
